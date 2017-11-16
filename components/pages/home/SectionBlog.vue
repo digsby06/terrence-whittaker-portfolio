24be617e517c4e6751920f491c15751e989d73a9
Terrence Whittaker <terr.whittakerjr@gmail.com>
2017-11-16 13:47:22 -0500

    Added feature image to blog

--- components/pages/home/SectionBlog.vue
+++ components/pages/home/SectionBlog.vue
@@ -11,8 +11,11 @@
   <div class="container--blogs cf w-100">
       <article v-if="!loading" v-for="blog of blogs" class="fl w-100 w-50-m w-25-ns pa2-ns mb4">
         <div class="aspect-ratio aspect-ratio--1x1">
-          <img style="background-image:url(http://mrmrs.github.io/images/0006.jpg);"
-          class="db bg-center cover aspect-ratio--object" />
+          <a :href="`blog/${blog.uid}`" class="link">
+            <img :style="{ 'background-image': 'url(' + blog.data.feature_image_sm.url + ')' }"
+            class="db bg-center cover aspect-ratio--object" />
+          </a>
+
         </div>
         <a :href="`blog/${blog.uid}`" class="pt3 ph2 ph0-ns pb3 link db">
           <small class="gray db pv2">{{getDate(blog.data.date)}}</small>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -7,7 +7,7 @@
           :page="page">
         </page-header>
 
-        <section class="pa5 ph7-l georgia mw9-l center cf">
+        <section class="pa5 ph7-l avenir mw9-l center cf">
 
 
             <article
@@ -16,12 +16,15 @@
               class="fl w-100 w-50-m w-50-ns pa4-ns flexbox-row flexbox-wrap">
 
               <div class="aspect-ratio aspect-ratio--1x1">
-                <img style="background-image:url(http://mrmrs.github.io/images/0006.jpg);"
-                class="db bg-center cover aspect-ratio--object" />
+                <a :href="`blog/${blog.uid}`" class="link">
+                  <img :style="{ 'background-image': 'url(' + blog.data.feature_image_sm.url + ')' }"
+                  class="db bg-center cover aspect-ratio--object" />
+                </a>
               </div>
 
 
               <a :href="`blog/${blog.uid}`"  class="pt3 ph2 ph0-ns pb3 link db">
+                <small class="gray db pv2">{{getDate(blog.data.date)}}</small>
                 <h3 v-for="headline in blog.data.headline" class="f5 f4-ns mb0 black-90">{{headline.text}}</h3>
                 <h3 v-for="sub_title in blog.data.sub_title" class="f6 f5 fw4 mt2 black-60">{{sub_title.text}}</h3>
               </a>
@@ -36,6 +39,7 @@
 
 <script>
 import {mapState} from 'vuex'
+import moment from 'moment'
 import PageHeader from '~/components/page-header/PageHeader'
 import Footer from '~/components/footer/Footer'
 
@@ -64,6 +68,11 @@ export default {
     ...mapState({
       blogs: state => state.blogs
     })
+  },
+  methods: {
+    getDate (date) {
+      return moment(date).format('ddd, DD MMMM YYYY')
+    }
   }
 }
 </script>
