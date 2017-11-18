398ce555f39acf03da8db523d4304e1d58a94744
Terrence Whittaker <terr.whittakerjr@gmail.com>
2017-11-18 10:46:47 -0500

    Removed junk and adjusted bg opacity

--- components/hero/Hero.vue
+++ components/hero/Hero.vue
@@ -1,12 +1,4 @@
 <template lang="html">
-<!-- <section class="bg-black-10 flex vh-100 items-center justify-end">
-  <div class="terrence--visible"></div>
-  <div class="tc tl-ns ph3 pa0-ns pa0-m mr5-m mr6-l mr7-ns bb-ns bw4-ns">
-      <h1 class="f1 f1-l fw6 fw7-ns mb2 mt4 mt0-ns avenir">Terrence Whittaker Jr.</h1>
-      <h2 class="f4 f3-l fw3 mb3 lh-title avenir">Front-End Developer & Creative</h2>
-  </div>
-</section> -->
-
 <home class="flex vh-100">
   <div class="leftside-bg bg-desktop">
     <img :src="`${bgImage}`">
@@ -53,6 +45,10 @@ export default {
     display: flex;
     align-items: flex-end;
     justify-content: flex-end;
+
+    img {
+      opacity: .65;
+    }
 }
 @media (max-width: 600px) {
     .hero {
@@ -96,18 +92,5 @@ export default {
   transform: translate(-50%,-50%);
   z-index: 2;
   text-align: left;
-}
-
-.terrence--visible {
-  position: absolute;
-  background-position: center;
-  background-image: url('~/assets/twBg.jpg');
-  background-repeat: no-repeat;
-  opacity: .3;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: -1000;
 }
 </style>
